e053c5af657fa2e5cf2df233863223ae44c00bd8
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-06-30 02:09:33 +0300

    added final touches and features

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ const audio6 = new Audio(encodeURI("Assets/F Single Note.mp3"));
 const audio7 = new Audio(encodeURI("Assets/Gb Single Note.mp3"));
 const audio8 = new Audio(encodeURI("Assets/G Single Note.mp3"));
 const audio9 = new Audio(encodeURI("Assets/Ab Single Note.mp3"));
+const loseAudio = new Audio(encodeURI("Assets/Villager Hit (Nr. 4 - Minecraft Sound) - Sound Effect for editing.mp3"))
 
 lives = 3;
-level=1;
-pattern=[];
-simmon=[];
+level = 1;
+pattern = [];
+simmon = [];
+clickable = false;
 
 var sounds = [audio1
     , audio2
@@ -26,72 +28,84 @@ var buttons = document.querySelectorAll("button");
 buttons.forEach(el => el.addEventListener('click', event => {
     index = [].indexOf.call(buttons, el);
     sounds[index].play();
-    setTimeout(()=>{
-        el.blur();
-    },900);
-    
-    if(document.getElementById("#click-cover")==null){
+    if (clickable) {
         pattern.push(index);
-        console.log(pattern,simmon);
-    }   
-    if(pattern.length>=simmon.length){
-        checkPattern();
+        console.log(pattern, simmon);
+        checkPattern(pattern.length - 1);
     }
+    setTimeout(() => {
+        el.blur();
+    }, 900);
 }));
 
 var hearts = document.querySelectorAll("img");
 
-$("#game-over").click(createSequence,reset);
-function reset(){
-    lives=3;
-    level=1;
-    pattern=[];
-    simmon=[];
-    document.getElementById("level").textContent="Level: " + level;
-    for (i=0;i<3;i++){
-        hearts[i].setAttribute("src","Assets/full_heart.png");
-    }
-}
-function createSequence(){
-    $("#game-over").toggle();
-    simmon=[]; 
-    for (i=0;i<level;i++){
-        simmon.push(Math.floor(Math.random()*9));
-    } 
-    playSequence(level);
-}
-function playSequence(iter){
-    if (iter<=0){
-        $("#click-cover").toggle();
-        return;
-    } 
-    playButton(parseInt(simmon[simmon.length-iter]));
-    setTimeout(()=>{playSequence(iter-1)},1500);
-}
-function playButton(number){
-    buttons[number].click();
-    buttons[number].focus();
-    setTimeout(()=>{
-        buttons[number].blur();
-    },900);
-}
-function checkPattern(){
-    if(pattern==simmon){
-        levelUp();
-    }else{
-        loseLife();
+$("#game-over").click(reset);
+
+function reset() {
+    $("#game-over").hide();
+    lives = 3;
+    level = 1;
+    pattern = [];
+    simmon = [];
+    document.getElementById("level").textContent = "Level: " + level;
+    for (i = 0; i < 3; i++) {
+        hearts[i].setAttribute("src", "Assets/full_heart.png");
     }
     createSequence();
 }
-function levelUp(){
-    level++;
-    document.getElementById("level").textContent=" Level: " + level;
+function createSequence() {
+    $("#click-cover").show();
+    clickable = false;
+    pattern = [];
+    simmon = [];
+    for (i = 0; i < level; i++) {
+        simmon.push(Math.floor(Math.random() * 9));
+    }
+    playSequence(level);
 }
-function loseLife(){
-    hearts[3-lives].setAttribute("src","Assets/empty_heart.png");
+function playSequence(iter) {
+    if (iter <= 0) {
+        $("#click-cover").hide();
+        clickable = true;
+        return;
+    }
+    playButton(parseInt(simmon[simmon.length - iter]));
+    setTimeout(() => { playSequence(iter - 1) }, 1500);
+}
+function playButton(number) {
+    buttons[number].click();
+    buttons[number].focus();
+    setTimeout(() => {
+        buttons[number].blur();
+    }, 900);
+}
+function checkPattern(index) {
+    if (pattern[index] != simmon[index]) {
+        loseLife();
+    } else if (pattern.length == simmon.length) {
+        levelUp();
+    }
+
+
+}
+function levelUp() {
+    level++;
+    document.getElementById("level").textContent = " Level: " + level;
+    setTimeout(() => { createSequence() }, 1500);
+}
+function loseLife() {
+    loseAudio.play();
+    pattern = [];
+    hearts[3 - lives].setAttribute("src", "Assets/empty_heart.png");
     lives--;
-    if (lives<=0){
-        $("#click-cover").toggle();
-        $("#game-over").toggle();
+    if (lives < 1) {
+        $("#click-cover").show();
+        $("#game-over").show();
+        clickable = false;
+    } else {
+        $("#click-cover").show();
+        clickable = false;
+        setTimeout(() => { playSequence(level) }, 1500);
     }
 }
