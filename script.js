fb5f08506e4cb1965c192722d9a1f01e96a2ec51
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-06-29 15:58:05 +0300

    configured audio and button presses

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-const audio1 = new Audio(encodeURI(Assets/C Single Note.mp3));
-const audio2 = new Audio(encodeURI(Assets/C# Db Single Note.mp3));
-const audio3 = new Audio(encodeURI(Assets/D Single Note.mp3));
-const audio4 = new Audio(encodeURI(Assets/D# Eb Single Note.mp3));
-const audio5 = new Audio(encodeURI(Assets/E Single Note.mp3));
-const audio6 = new Audio(encodeURI(Assets/F Single Note.mp3));
-const audio7 = new Audio(encodeURI(Assets/F# Gb Single Note.mp3));
-const audio8 = new Audio(encodeURI(Assets/G Single Note.mp3));
-const audio9 = new Audio(encodeURI(Assets/G# Ab Single Note.mp3));
+const audio1 = new Audio(encodeURI("Assets/C Single Note.mp3"));
+const audio2 = new Audio(encodeURI("Assets/Db Single Note.mp3"));
+const audio3 = new Audio(encodeURI("Assets/D Single Note.mp3"));
+const audio4 = new Audio(encodeURI("Assets/Eb Single Note.mp3"));
+const audio5 = new Audio(encodeURI("Assets/E Single Note.mp3"));
+const audio6 = new Audio(encodeURI("Assets/F Single Note.mp3"));
+const audio7 = new Audio(encodeURI("Assets/Gb Single Note.mp3"));
+const audio8 = new Audio(encodeURI("Assets/G Single Note.mp3"));
+const audio9 = new Audio(encodeURI("Assets/Ab Single Note.mp3"));
+
+var sounds = [audio1
+    , audio2
+    , audio3
+    , audio4
+    , audio5
+    , audio6
+    , audio7
+    , audio8
+    , audio9];
+var buttons = document.querySelectorAll("button");
+buttons.forEach(el => el.addEventListener('click', event => {
+    index = [].indexOf.call(buttons, el);
+    sounds[index].play();
+}));
