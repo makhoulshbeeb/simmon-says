ff97368089ff4f7a2fc04a9e1168483390c9882c
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-06-30 01:10:59 +0300

    added various functionalities and game mechanics

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const audio7 = new Audio(encodeURI("Assets/Gb Single Note.mp3"));
 const audio8 = new Audio(encodeURI("Assets/G Single Note.mp3"));
 const audio9 = new Audio(encodeURI("Assets/Ab Single Note.mp3"));
 
+lives = 3;
+level=1;
+pattern=[];
+simmon=[];
+
 var sounds = [audio1
     , audio2
     , audio3
@@ -21,4 +26,72 @@ var buttons = document.querySelectorAll("button");
 buttons.forEach(el => el.addEventListener('click', event => {
     index = [].indexOf.call(buttons, el);
     sounds[index].play();
+    setTimeout(()=>{
+        el.blur();
+    },900);
+    
+    if(document.getElementById("#click-cover")==null){
+        pattern.push(index);
+        console.log(pattern,simmon);
+    }   
+    if(pattern.length>=simmon.length){
+        checkPattern();
+    }
 }));
+
+var hearts = document.querySelectorAll("img");
+
+$("#game-over").click(createSequence,reset);
+function reset(){
+    lives=3;
+    level=1;
+    pattern=[];
+    simmon=[];
+    document.getElementById("level").textContent="Level: " + level;
+    for (i=0;i<3;i++){
+        hearts[i].setAttribute("src","Assets/full_heart.png");
+    }
+}
+function createSequence(){
+    $("#game-over").toggle();
+    simmon=[]; 
+    for (i=0;i<level;i++){
+        simmon.push(Math.floor(Math.random()*9));
+    } 
+    playSequence(level);
+}
+function playSequence(iter){
+    if (iter<=0){
+        $("#click-cover").toggle();
+        return;
+    } 
+    playButton(parseInt(simmon[simmon.length-iter]));
+    setTimeout(()=>{playSequence(iter-1)},1500);
+}
+function playButton(number){
+    buttons[number].click();
+    buttons[number].focus();
+    setTimeout(()=>{
+        buttons[number].blur();
+    },900);
+}
+function checkPattern(){
+    if(pattern==simmon){
+        levelUp();
+    }else{
+        loseLife();
+    }
+    createSequence();
+}
+function levelUp(){
+    level++;
+    document.getElementById("level").textContent=" Level: " + level;
+}
+function loseLife(){
+    hearts[3-lives].setAttribute("src","Assets/empty_heart.png");
+    lives--;
+    if (lives<=0){
+        $("#click-cover").toggle();
+        $("#game-over").toggle();
+    }
+}
